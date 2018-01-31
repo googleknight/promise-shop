9387dbf57ec57c887bb5e19efdf21b4c5f4b3d46
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-31 12:21:32 +0530

    important-rule: Tested returned value of iterate

--- importantrule/importantrule.test.js
+++ importantrule/importantrule.test.js
@@ -1,5 +1,10 @@
 const importantrule = require('./importantrule');
 
+describe('Testing iterate', () => {
+  it('It should return 2 on passing 1', () => {
+    expect(importantrule.iterate(1)).toBe(2);
+  });
+});
 describe('Testing chainedpromise  ', () => {
   it('The returned value is Promise ', () => {
     expect(importantrule.chainedPromises()).toBeInstanceOf(Promise);
